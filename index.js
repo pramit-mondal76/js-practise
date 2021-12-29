926092163b8eb55c4916584fb01ca25f3d5b05c8
pramit-mondal <mondalpramit76@gmail.com>
2021-12-29 21:54:17 +0530

    js

---
+++ index.js
@@ -0,0 +1,36 @@
+ //var myname='pramit mondal';
+// var myage=18;
+// console.log(myage);
+// var iampramit=true;
+// console.log(typeof(myname));
+// console.log(typeof(iampramit));
+// console.log(true+true);
+// console.log(false-true);
+// var iamuseless=null;
+// console.log(typeof(iamuseless));//bug
+// console.log(10+"20");
+// console.log(9-"5");//bug
+//nan-property of global object.
+// var myphonenumber=9832102128
+// console.log(isNaN(myphonenumber));//not a number-false
+// var myname='pramit';
+// console.log(isNaN(myname));//not a number true
+// if(isNaN(myname))
+// {
+//     console.log("pleasae enter a valid phone number.");
+// }
+console.log(10+20);//10 and 20 are operand and +  is operator.
+//assingment operator -assign a value to its left operand
+// var x=5;
+// var y=5;
+// console.log("is both the x and y are equal or not"+x==y);//false
+// console.log(x==y);//true
+// //arithmatic operator-
+// console.log(9+7);
+// console.log(9-7);
+// console.log(9*7);
+// console.log(49/7);
+// console.log(4%7);
+
+
+//increament and decreament operator-
