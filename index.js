d4a733d8f97685d00711ba6063ecd59a6f9a67e1
pramit-mondal <mondalpramit76@gmail.com>
2022-01-15 21:35:04 +0530

    new

--- index.js
+++ index.js
@@ -50,8 +50,76 @@
 // console.log(a<b);
 
 //logical operator-typically used with boolean (logical )values,when they are they return a boolean value.
-var a= 30;
-var b= -20;
-console.log(a>b && b>0);//logical and operartor-if aany of the expression is false then output gives false and if those two are ture then it gives true .
-console.log(a>b || b>0);//logical or operator-if one of condition is true then the output will be true.
+// var a= 30;
+// var b= -20;
+// console.log(a>b && b>0);//logical and operartor-if aany of the expression is false then output gives false and if those two are ture then it gives true .
+//console.log(a>b || b>0);//logical or operator-if one of condition is true then the output will be true.
 //console.log(a);
+//string-
+//concatination operator- +
+// console.log("hello "+"world")
+// console.log(5**2)
+// console.log(5 +"pramit")//concatinate
+// console.log(5- "pramit")//output will be NaN-Not a number
+// var n1=5;
+// var n2='5';
+// console.log(typeof(n1))
+// console.log(typeof(n2))
+// console.log(n1 == n2)//== check only value
+// console.log(n1 === n2)//=== check value as well as data type
+
+//control statement and loop-
+//1.if-else=
+// var n="pramit"
+// if(n =="prami")
+// {
+//    console.log("wright");
+// }else{
+//     console.log("wrong");
+// }
+//Functoin-
+// function sum(a,b)
+// {
+//  var total=a+b;
+//  console.log(total);
+// }
+// sum(45,46)
+// sum();//NaN
+
+//anonymous function-the function which has not any name .
+// var funcexp= function(a,b)
+// {
+//     return total =a+b;
+// }
+// console.log(funcexp(5,9));
+// console.log('the sum of two number is '+ funcexp(5,34));
+
+
+
+//ECMAScript---------------
+
+
+//1.LET VS CONST VS VAR
+// var = function scope
+// const and let =block scope
+
+//----------var--------
+
+// var  myname="pramit"
+// console.log(myname)
+// myname= "rimo"
+// console.log(myname)
+
+//----------let--------
+
+// let  myname="pramit"
+// console.log(myname)
+// myname= "rimo"
+// console.log(myname)
+
+//----------const------
+
+// const myname="pramit"//const is not changeable
+// console.log(myname)
+// myname= "rimo"
+// console.log(myname)
