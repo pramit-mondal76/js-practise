2da3d7b33fe4b66c041985eee9cf66ec8d5ddeee
pramit-mondal <mondalpramit76@gmail.com>
2022-01-30 09:06:07 +0530

    new

---
+++ js.js
@@ -0,0 +1,108 @@
+//java script console api------
+//console.log("pramit Mondal");
+//document.write("This is document write")
+//console.warn("this is warnning")
+//console.error("This is an error.")
+
+//js varriable---
+//containers to store data values
+//numbers-
+var n1=34;
+var n2=45;
+//console.log(n1+n2);
+
+//string-
+var str1="Pramit";
+var str2="Mondal";
+
+//objects-
+var marks={
+    ram:10, sam:20, jadu:40
+}
+//console.log(marks);
+
+//booleaans-
+var a=true;
+var b=false;
+
+//udefined and null-
+var z;
+//console.log(z);
+var x=null;
+//console.log(x);
+
+/*
+At a very high level there are two types pf data types in javascript
+1.primitive data types:undefined,null,number,string,boolean,symbol
+2.reference data types:arrays and object
+
+*/
+
+//array--
+var array=[1,2,"pramit",3,4];
+
+//operators-----
+//1.Arithmatics operator----[+,-,*,/]
+var a= 30;
+var b=15;
+/*console.log("The value of a+b is", a+b);
+console.log("The value of a-b is", a-b);
+console.log("The value of a*b is", a*b);
+console.log("The value of a/b is", a/b);*/
+
+//2.assingment operators--
+var x=10;
+var y=x;
+//console.log(y);
+
+//comparison operator--
+var p=10;
+var q=12;
+/*console.log(p==q);
+console.log(p>=q);
+console.log(p<=q);
+console.log(p<q);
+console.log(p>q);*/
+
+
+//Logical operators---
+
+//1.AND opeartor-
+/*console.log(true && true);
+console.log(true && false);
+console.log(false && false);*/
+
+//2.OR operator-
+/*console.log(true || true);
+console.log(true || false);
+console.log(false || false);*/
+
+//3.NOT operator-
+/*console.log(!false);
+console.log(!true);*/
+
+
+
+//FUNCTION-----
+
+/*function sum(a,b)
+{
+    return a+b;
+}
+s=sum(12,12);
+console.log(s);*/
+
+//conditionals in javascript---
+/*var age=10;
+if(age<=10)
+{
+    console.log("kid");
+}
+else if(age>10 && age <=18)
+{
+    console.log("adult");
+}
+else
+{
+    console.log("young");
+}*/
